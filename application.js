be2ea05f7ed9114b786679d6ed16e8614c2efff6
jdcar <jdcarlstone@gmail.com>
2021-01-30 15:14:42 -0600

    Start questions

---
+++ application.js
@@ -0,0 +1,33 @@
+
+
+const questions = [
+    {
+        type: 'checkbox',
+        message: 'What would you like to do?',
+        name: "todo",
+        choices: [
+
+            // Create
+            'Add Department',
+            'Add Role',
+            'Add Employee',
+            // Read
+            'View all Departments',
+            'View all Roles',
+            'View all employees',
+            // Update
+            'Update Employee Role',
+
+            // Bonus
+            // 'Update Employee Manager',  
+            // 'View all Employees by Manager', 
+            // 'View All Employees by Department', 
+            // 'Remove Department',
+            // 'Remove Role',
+            // 'Remove Employee', 
+        
+        ]
+
+    },
+  
+]
